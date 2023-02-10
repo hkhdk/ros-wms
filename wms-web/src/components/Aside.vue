c63d1f8d555184bfc3278a3d0a795af63f7a5218
书启秋枫 <1327430675@qq.com>
2023-02-10 21:01:05 +0800

    仓库管理系统(SpringBoot+Vue)

---
+++ wms-web/src/components/Aside.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-menu
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      style="height: 100vh;"
+      default-active="/Home"
+      :collapse="isCollapse"
+      :collapse-transition="false">
+
+    <el-menu-item index="/Home">
+      <i class="el-icon-s-fold"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+
+    <el-menu-item index="/One">
+      <i class="el-icon-s-flag"></i>
+      <span slot="title">导航一</span>
+    </el-menu-item>
+
+    <el-menu-item index="/Two">
+      <i class="el-icon-s-flag"></i>
+      <span slot="title">导航二</span>
+
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Aside",
+  data() {
+    return {
+      // isCollapse: false
+    }
+  },
+  props: {
+    isCollapse: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ wms-web/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="display: flex;line-height: 60px">
+    <div style="margin-top: 8px;">
+      <i :class="icon" style="font-size: 20px;cursor: pointer;" @click="collapse"></i>
+    </div>
+    <div style="flex: 1;text-align: center;font-size: 34px;">
+      <span>欢迎来到仓库管理系统</span>
+    </div>
+    <el-dropdown>
+      <span>王小虎</span>
+      <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
+        <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    icon: String
+  },
+  methods: {
+    toUser() {
+      alert("个人中心详情")
+    },
+    logout() {
+      alert("您已退出")
+    },
+    collapse() {
+      this.$emit('doCollapse')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

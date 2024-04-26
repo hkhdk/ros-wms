46b8e7d150252def41b88efab3d1205822babaf2
hkhdk <2066760980@qq.com>
2024-04-26 23:11:48 +0800

    fixed

--- wms-web/vue.config.js
+++ wms-web/vue.config.js
@@ -1,5 +1,13 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    lintOnSave: false
+    // lintOnSave: false
+    devServer: {
+        proxy:{
+          '/wms':{
+            target:'http://192.168.0.100',
+            changeOrigin:true
+          }
+        }
+      }
 })
